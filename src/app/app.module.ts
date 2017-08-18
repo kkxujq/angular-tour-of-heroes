958259b946c6bc96294e441702f3f7f839dd6a8c
kkxujq <kkxujq.163.com>
2017-08-18 18:45:08 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
 import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
