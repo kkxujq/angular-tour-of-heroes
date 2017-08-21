b6d8c02644bcbf42938c8d9b05d2199612c1d770
kkxujq <kkxujq@163.com>
2017-08-21 20:03:15 +0800

    8/21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,17 @@
-import { Component, OnInit} from '@angular/core';
-
-import { HeroService} from './hero.service';
-
-import { Hero } from './hero';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+    selector: 'my-app',
+    template: `
+      <h1>{{title}}</h1>
+      <nav>
+        <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+        <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+      </nav>
+      <router-outlet></router-outlet>
+    `,
+    styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
-
-  constructor(private heroService: HeroService) { }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+    title = 'Tour of Heroes';
 }
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,24 +1,38 @@
-/**
- * Created by jq on 2017/8/18.
- */
-import { Component, Input } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
 import { Hero } from './hero';
+
+import { HeroService } from './hero.service';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'hero-detail',
   template: `
     <div *ngIf="hero">
       <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>id: </label>{{hero.id}}</div>
       <div>
         <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
+        <input [(ngModel)]="hero.name" placeholder="name" />
       </div>
+      <button (click)="goBack()">Back</button>
     </div>
   `
 })
 
+
 export class HeroDetailComponent {
   @Input() hero: Hero;
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  goBack(): void {
+    this.location.back();
+  }
 }
